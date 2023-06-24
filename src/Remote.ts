b0657528ad6e8b71451e7217823f7a67e277509e
h0rzn <hrzn@protonmail.com>
2023-06-25 00:34:59 +0200

    began adding webrtc logic

---
+++ src/Remote.ts
@@ -0,0 +1,33 @@
+class Remote {
+    localCon: RTCPeerConnection;
+    sendChan: RTCDataChannel;
+
+    remoteCon: RTCPeerConnection;
+
+    constructor() {
+        // local
+        this.localCon = new RTCPeerConnection();
+        this.sendChan = this.localCon.createDataChannel("sendChannel");
+        this.sendChan.onopen = this.onSendChannelStatusChange;
+        this.sendChan.onclose = this.onSendChannelStatusChange;
+
+        // remote
+        this.remoteCon = new RTCPeerConnection();
+        this.remoteCon.ondatachannel = this.onReceiveChannel;
+
+
+        // ice
+    }
+
+    offerConnection() {
+
+    }
+
+    onSendChannelStatusChange() {}
+
+    onReceiveChannel() {}
+
+    send() {}
+
+
+}
